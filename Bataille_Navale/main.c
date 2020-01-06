527ca763c18f36a8590d8fd2b4d9e48a45740752
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-01-06 12:20:50 +0100

    Début de la bataille Navale.

---
+++ Bataille_Navale/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Bonjour et Bienvenue dans ce jeu de Bataille Navale !\nNous allons commencer avec le Joueur 1, Joueur 2 veuillez laisser lui laisser la machine.\nAppuyez sur n'importe quelle touche quand c'est bon.");
+    getchar();
+    fflush(stdin);
+    system("cls");
+
+    return 0;
+}
